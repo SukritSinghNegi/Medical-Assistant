fde39e856d06524271babdcd3b9ef9eb5615f3d8
Sukrit Negi <sukritnegi@gmail.com>
2024-08-11 23:55:42 +0530

    fix

--- api/setup.py
+++
@@ -1,9 +0,0 @@
-from setuptools import setup, find_packages
-
-setup(name="Medical-Chatbot",
-      version= "0.0.1",
-      author="Sukrit",
-      author_email="sukritnegi@gmail.com",
-      packages= find_packages(),
-      install_requires=["flask","python-dotenv", "pandas","bs4","logging","pymongo","flask_cors","requests","langchain","langchain-community","langchain_core","langchain-astradb","google-generativeai","langchain-google-genai"]
-)
